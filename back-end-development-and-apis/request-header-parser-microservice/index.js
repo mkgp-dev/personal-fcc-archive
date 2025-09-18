718066df8d7d39fe11e722569accc0174f99ab1c
Mark Pelayo <mark.pelayo014@gmail.com>
2025-09-18 17:10:47 +0800

    Files for verification

---
+++ back-end-development-and-apis/request-header-parser-microservice/index.js
@@ -0,0 +1,46 @@
+// index.js
+// where your node app starts
+
+// init project
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+// @mkgp-dev
+app.set('trust proxy', true);
+
+app.get('/api/whoami', (req, res) => {
+  const ip = req.ip;
+  const lang = req.get('accept-language');
+  const useragent = req.get('user-agent');
+  let response = {
+    ipaddress: ip,
+    language: lang,
+    software: useragent
+  };
+
+  res.json(response);
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});

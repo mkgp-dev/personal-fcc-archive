5531419c98bf8a7658b01d5bc8cd1c1dddb0564b
Mark Pelayo <mark.pelayo014@gmail.com>
2025-10-02 16:04:39 +0800

    wrong date, fixed

--- daily-coding-challenge/10012025_binary-to-decimal.js
+++ daily-coding-challenge/10012025_binary-to-decimal.js
@@ -1,5 +1,5 @@
 /*
-(October 02, 2025) Binary to Decimal
+(October 01, 2025) Binary to Decimal
 Given a string representing a binary number, return its decimal equivalent as a number.
 
 A binary number uses only the digits 0 and 1 to represent any number. To convert binary to decimal, multiply each digit by a power of 2 and add them together. Start by multiplying the rightmost digit by 2^0, the next digit to the left by 2^1, and so on. Once all digits have been multiplied by a power of 2, add the result together.

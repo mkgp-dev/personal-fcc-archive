f758348db92c132ae5eb23b4f3b2a57b3d99edaf
Mark Pelayo <mark.pelayo014@gmail.com>
2025-10-04 12:00:18 +0800

    10032025_log

---
+++ daily-coding-challenge/October 2025/10032025_password-strength.js
@@ -0,0 +1,54 @@
+/*
+(October 03, 2025) P@ssw0rd Str3ngth!
+Given a password string, return "weak", "medium", or "strong" based on the strength of the password.
+
+A password is evaluated according to the following rules:
+It is at least 8 characters long.
+It contains both uppercase and lowercase letters.
+It contains at least one number.
+It contains at least one special character from this set: !, @, #, $, %, ^, &, or *.
+
+Return "weak" if the password meets fewer than two of the rules. Return "medium" if the password meets 2 or 3 of the rules. Return "strong" if the password meets all 4 rules.
+
+To test:
+checkStrength("123456")
+checkStrength("PassWord%^!")
+checkStrength("C0d3&Fun!")
+*/
+
+function checkStrength(password) {
+  // variables
+  let pass = 0;
+  let response;
+
+  // criteria
+  const length = /^.{8,}$/;
+  const letter = /^(?=.*[A-Z])(?=.*[a-z]).+$/;
+  const number = /^(?=.*[\d]).+$/;
+  const charac = /^(?=.*[!@#$%^&*]).+$/;
+
+  // It is at least 8 characters long.
+  if (length.test(password)) pass++;
+
+  // It contains both uppercase and lowercase letters.
+  if (letter.test(password)) pass++;
+
+  // It contains at least one number.
+  if (number.test(password)) pass++;
+
+  // It contains at least one special character from this set [!, @, #, $, %, ^, &, or *.].
+  if (charac.test(password)) pass++;
+
+  // verify
+  if (pass < 2) {
+    response = 'weak';
+  } else if (pass === 2 || pass <= 3) {
+    response = 'medium';
+  } else if (pass === 4) {
+    response = 'strong';
+  }
+
+  // results
+  // debug purposes console.log(pass);
+  return response;
+}
